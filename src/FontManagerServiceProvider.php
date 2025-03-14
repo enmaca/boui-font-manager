196bdf87a8ef1a4d182296b80672936a3c08682a
Enrique Martinez <enmaca@users.noreply.github.com>
2025-03-14 13:57:21 -0600

    14-03-25: Update

--- src/FontManagerServiceProvider.php
+++ src/FontManagerServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Enmaca\Backoffice\FontManager;
 
+use Enmaca\Backoffice\FontManager\UI\MasterLayoutSideMenu;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\ServiceProvider;
 use ReflectionException;
@@ -14,13 +15,36 @@ class FontManagerServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+
+        if (class_exists('App\Services\UxmalSkeletonAppService') && !$this->app->bound('App\Services\UxmalSkeletonAppService')) {
+            $this->app->singleton(\App\Services\UxmalSkeletonAppService::class, function () {
+                return new \App\Services\UxmalSkeletonAppService();
+            });
+        } else {
+            // Handle the case where UxmalSkeletonAppService does NOT exist
+            // Log an error, or any other fallback mechanism
+            // Example:
+            \Log::warning('UxmalSkeletonAppService class not found. Skipping registration.');
+        }
+
+
         app()->make(RegisterCmdQry::class)->register(__DIR__.'/Domains/', [
             // 'middleware' => ['auth:sanctum'],
         ]);
     }
 
+    /**
+     * @throws BindingResolutionException
+     */
     public function boot(): void
     {
+        if (class_exists('App\Services\UxmalSkeletonAppService') && $this->app->bound('App\Services\UxmalSkeletonAppService')) {
+            $uxmalService = $this->app->make(\App\Services\UxmalSkeletonAppService::class);
+            if (method_exists($uxmalService, 'register')) {
+                $uxmalService->register(MasterLayoutSideMenu::get());
+            }
+        }
+
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
 
